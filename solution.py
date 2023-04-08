db1945fa158294ddf62829d9c1d6e6f0893895b2
likalikabaz <89534939+likalikabaz@users.noreply.github.com>
2023-04-08 18:14:02 +0300

    Update solution.py

--- solution.py
+++ solution.py
@@ -1,11 +1,43 @@
-import pandas as pd
 import numpy as np
 
-
-chat_id = 123456 # Ваш chat ID, не меняйте название переменной
-
+chat_id = 753540521 
+# https://shitpoet.cc/sshots/2023-04-07--193056--196905498.png
 def solution(x: np.array) -> float:
-    # Измените код этой функции
-    # Это будет вашим решением
-    # Не меняйте название функции и её аргументы
-    return x.mean() # Ваш ответ
+    offset = 791
+    i = 0
+    while i < len(x):
+        x[i] = x[i] - offset
+        i = i + 1
+    # a_x - expected value of `x`, average over `x`
+    sum = 0
+    i = 0
+    while i < len(x):
+        sum = sum + x[i]
+        i = i + 1
+    average_x = sum / len(x)
+    
+    i = 0
+    y = []
+    while i < len(x):
+        y.append(x[i] - average_x)
+        i = i + 1
+    i = 0    
+    while i < len(y):
+        y[i] = y[i] * y[i]
+        i = i + 1
+    sum = 0    
+    i = 0
+    while i < len(y):
+        sum = sum + y[i]
+        i = i + 1
+        
+    variance_x = sum / len(y)
+    
+    # print(x)
+    # print(average_x)
+    # print(variance_x)
+    # print(y)
+    
+    average = math.log((average_x ** 2) / math.sqrt(average_x ** 2 + variance_x ** 2))
+
+    return average # Ваш ответ
